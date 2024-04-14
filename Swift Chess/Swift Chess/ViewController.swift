10f82d9c87c4dd0e37c489e7ac23a395cc860025
Miguel Duran <mduranr@falabella.cl>
2024-04-14 17:56:03 -0400

    Add empty project

---
+++ Swift Chess/Swift Chess/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swift Chess
+//
+//  Created by Miguel Duran on 14-04-24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
