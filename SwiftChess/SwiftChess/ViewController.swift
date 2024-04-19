f184858e35d8acd0fab91a5c2907c98bdbebfed1
Miguel Duran <mduranr@falabella.cl>
2024-04-19 16:05:23 -0400

    Configure project to use UIKit in code

--- SwiftChess/SwiftChess/ViewController.swift
+++ SwiftChess/SwiftChess/ViewController.swift
@@ -9,50 +9,11 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
         
-        override func loadView() {
-            super.loadView()
-            debugPrint("loadView")
-        }
-        
-        override func viewDidLoad() {
-            super.viewDidLoad()
-            debugPrint("viewDidLoad")
-        }
-        
-        override func viewWillAppear(_ animated: Bool) {
-            super.viewWillAppear(animated)
-            debugPrint("viewWillAppear")
-        }
-        
-        override func viewWillLayoutSubviews() {
-            super.viewWillLayoutSubviews()
-            debugPrint("viewWillLayoutSubviews")
-        }
-        
-        override func viewDidLayoutSubviews() {
-            super.viewDidLayoutSubviews()
-            debugPrint("viewDidLayoutSubviews")
-        }
-        
-        override func viewDidAppear(_ animated: Bool) {
-            super.viewDidAppear(animated)
-            debugPrint("viewDidAppear")
-        }
-        
-        override func viewDidDisappear(_ animated: Bool) {
-            super.viewDidDisappear(animated)
-            debugPrint("viewDidDisappear")
-        }
-        
-        deinit {
-            debugPrint("deinit - ViewController")
-        }
-        
-        override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
-            debugPrint("viewWillTransition")
-        }
+        view.backgroundColor = .systemIndigo
     }
 }
 
