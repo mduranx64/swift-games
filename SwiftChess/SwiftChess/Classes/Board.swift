e820481773d99036366d3b1a90ce3ad5c2e34e27
Miguel Duran <mduranr@falabella.cl>
2024-04-19 15:22:05 -0400

    Add board with positions

--- SwiftChess/SwiftChess/Classes/Board.swift
+++ SwiftChess/SwiftChess/Classes/Board.swift
@@ -8,6 +8,22 @@
 import Foundation
 
 public class Board {
+    
+    private(set) var pieces: [[Piece?]] = [
+        [Piece(.rook, color: .black), Piece(.knight, color: .black), Piece(.bishop, color: .black), Piece(.queen, color: .black),
+         Piece(.king, color: .black), Piece(.bishop, color: .black), Piece(.knight, color: .black), Piece(.rook, color: .black)],
+        [Piece(.pawn, color: .black), Piece(.pawn, color: .black), Piece(.pawn, color: .black), Piece(.pawn, color: .black),
+         Piece(.pawn, color: .black), Piece(.pawn, color: .black), Piece(.pawn, color: .black), Piece(.pawn, color: .black)],
+        [nil, nil, nil, nil, nil, nil, nil, nil],
+        [nil, nil, nil, nil, nil, nil, nil, nil],
+        [nil, nil, nil, nil, nil, nil, nil, nil],
+        [nil, nil, nil, nil, nil, nil, nil, nil],
+        [Piece(.pawn, color: .white), Piece(.pawn, color: .white), Piece(.pawn, color: .white), Piece(.pawn, color: .white),
+         Piece(.pawn, color: .white), Piece(.pawn, color: .white), Piece(.pawn, color: .white), Piece(.pawn, color: .white)],
+        [Piece(.rook, color: .white), Piece(.knight, color: .white), Piece(.bishop, color: .white), Piece(.queen, color: .white),
+         Piece(.king, color: .white), Piece(.bishop, color: .white), Piece(.knight, color: .white), Piece(.rook, color: .white)]
+    ]
+    
     init() {
         
     }
--- SwiftChess/SwiftChessTests/Classes/BoardTests.swift
+++ SwiftChess/SwiftChessTests/Classes/BoardTests.swift
@@ -6,9 +6,61 @@
 //
 
 import XCTest
+@testable import SwiftChess
 
 final class BoardTests: XCTestCase {
+    
+    func testBoardInit() {
+        XCTAssertNotNil(makeSUT())
+    }
+    
+    func testBoardPiecesCount() {
+        XCTAssertEqual(makeSUT()?.pieces.count, 8)
+        XCTAssertEqual(makeSUT()?.pieces[0].count, 8)
+        XCTAssertEqual(makeSUT()?.pieces[1].count, 8)
+        XCTAssertEqual(makeSUT()?.pieces[2].count, 8)
+        XCTAssertEqual(makeSUT()?.pieces[3].count, 8)
+        XCTAssertEqual(makeSUT()?.pieces[4].count, 8)
+        XCTAssertEqual(makeSUT()?.pieces[5].count, 8)
+        XCTAssertEqual(makeSUT()?.pieces[6].count, 8)
+        XCTAssertEqual(makeSUT()?.pieces[7].count, 8)
+    }
+    
+    func testBoardPiecesPositionsAndColors() {
+        let pieces: [[PieceType?]] = [
+            [.rook, .knight, .bishop, .queen, .king, .bishop, .knight, .rook],
+            [.pawn, .pawn, .pawn, .pawn, .pawn, .pawn, .pawn, .pawn],
+            [nil, nil, nil, nil, nil, nil, nil, nil],
+            [nil, nil, nil, nil, nil, nil, nil, nil],
+            [nil, nil, nil, nil, nil, nil, nil, nil],
+            [nil, nil, nil, nil, nil, nil, nil, nil],
+            [.pawn, .pawn, .pawn, .pawn, .pawn, .pawn, .pawn, .pawn],
+            [.rook, .knight, .bishop, .queen, .king, .bishop, .knight, .rook]
+            
+        ]
+        
+        let colors: [[PieceColor?]] = [
+            [.black, .black, .black, .black, .black, .black, .black, .black],
+            [.black, .black, .black, .black, .black, .black, .black, .black],
+            [nil, nil, nil, nil, nil, nil, nil, nil],
+            [nil, nil, nil, nil, nil, nil, nil, nil],
+            [nil, nil, nil, nil, nil, nil, nil, nil],
+            [nil, nil, nil, nil, nil, nil, nil, nil],
+            [.white, .white, .white, .white, .white, .white, .white, .white],
+            [.white, .white, .white, .white, .white, .white, .white, .white]
+            
+        ]
+        let boardPieces = makeSUT().pieces
+        for (i, row) in boardPieces.enumerated() {
+            for (j, item) in row.enumerated() {
+                XCTAssertEqual(item?.type, pieces[i][j], "\(i) \(j)")
+                XCTAssertEqual(item?.color, colors[i][j], "\(i) \(j)")
+            }
+        }
+    }
 
 
-
+    private func makeSUT() -> Board! {
+        return Board()
+    }
 }
