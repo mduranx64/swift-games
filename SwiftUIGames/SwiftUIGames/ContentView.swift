5e64e4cbe3043b90388d075eb20e881124049d97
Miguel Duran <mduranr@falabella.cl>
2024-09-04 22:07:09 -0500

    Add empty swift ui project

---
+++ SwiftUIGames/SwiftUIGames/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  SwiftUIGames
+//
+//  Created by Miguel Duran on 04-09-24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+
+    var body: some View {
+        Image(uiImage: .squareGrayDark)
+        Image(uiImage: .squareGrayLight)
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUIGames/SwiftUIGames/SwiftUIGamesApp.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIGamesApp.swift
+//  SwiftUIGames
+//
+//  Created by Miguel Duran on 04-09-24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIGamesApp: App {
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
